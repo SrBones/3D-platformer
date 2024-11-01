044b0ebd4ee019f3996bb4e49b85ea2d4aff1c0f
Sevxn <nataliakrakowska6@gmail.com>
2024-11-01 11:20:36 +0100

    WIP

---
+++ Assets/Scripts/Moving Platform.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float maxRotation = 30.0f;
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, maxRotation * Mathf.Sin(Time.time * speed));
+        //transform.rotation = Quaternion.Euler(0.0f, 0.0f, maxRotation * (Mathf.PingPong(Time.time * speed, 2.0f)-1.0f));
+    }
+}
