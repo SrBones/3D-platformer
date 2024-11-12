b5164a84891a881dcbc1d7399b2d99196a40886a
SrBones <benjamin.rekdal.sjoberg@gmail.com>
2024-11-12 05:58:09 +0100

    Main menu + mer
    Lagt til:
    Main menu med start og quit button (TRENGER UI DESIGN)
    Invisible walls på level 1
    trær og vann level 1
    Dropshadow på spiller i level 1 og 2

---
+++ Assets/Scripts/BlobShadow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobShadow : MonoBehaviour
+{
+    public GameObject shadow;
+    public RaycastHit hit;
+    public float offset;
+
+    private void FixedUpdate()
+    {
+        Ray downRay = new Ray(new Vector3(this.transform.position.x, this.transform.position.y - offset, this.transform.position.z), -Vector3.up);
+
+        Vector3 hitPosition = hit.point;
+        shadow.transform.position = hitPosition;
+
+        if(Physics.Raycast(downRay, out hit))
+        {
+            print(hit.transform);
+        }
+    }
+}
